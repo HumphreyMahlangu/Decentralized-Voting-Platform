c0a3b64ea829f87107c37a724601041aa158d66c
HumphreyMahlangu <godasamukelo757@gmai.com>
2026-07-22 20:06:37 +0200

    Initialize Decentralized Voting Platform project with Maven setup and basic application structure

---
+++ src/main/java/decentralizedvotingplatform/DecentralizedVotingPlatformApplication.java
@@ -0,0 +1,13 @@
+package decentralizedvotingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecentralizedVotingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecentralizedVotingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/decentralizedvotingplatform/DecentralizedVotingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package decentralizedvotingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecentralizedVotingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
